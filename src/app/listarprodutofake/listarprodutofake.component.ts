070d93fe84fb2899d761be4373db847ea8562e0d
Marcelo Augusto Pickler <116655836+patomanco@users.noreply.github.com>
2024-06-27 21:21:48 -0300

    adicionado listagem de produtos

--- src/app/listarprodutofake/listarprodutofake.component.ts
+++ src/app/listarprodutofake/listarprodutofake.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
 import { FakeprodutoService } from '../fakeproduto.service';
+import { NgFor } from '@angular/common'
 
 @Component({
   selector: 'app-listarprodutofake',
   standalone: true,
-  imports: [],
+  imports: [ NgFor ],
   templateUrl: './listarprodutofake.component.html',
   styleUrl: './listarprodutofake.component.css'
   
 })
 export class ListarprodutofakeComponent {
-  
-  produtos: any[] = []
-  constructor(private fakeProduto: FakeprodutoService){}
+  produtos: any[] = [{id:1, title:'milho',price:10}]
+
+  constructor(private fakeProduto: FakeprodutoService){
+     this.carregarProdutos()
+  }
 
   carregarProdutos(){
-    this.fakeProduto.getProdutos().subscribe(dados => console.log(dados))
+    this.fakeProduto.getProdutos().subscribe(dados => this.produtos = dados)      
   }
 }
